2e23f63f0172d9db350e2d0f27786e04167d36e3
Israel Zorzor <46909353+Hammondziggy@users.noreply.github.com>
2022-11-06 19:15:42 +0000

    added error boundary

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react'
 import './App.css'
 import {BrowserRouter as Router, Routes, Route, NavLink, useNavigate} from 'react-router-dom'
+import {ErrorBoundary} from 'react-error-boundary'
+import Fallback from './Fallback'
 import Home from './pages/Home'
 import About from './pages/About'
 import Resume from './pages/Resume'
@@ -13,19 +15,27 @@ import Navbar from './components/Navbar'
 import {RepoDetails} from './pages/RepoDetails'
 
 export default function App () {
+
+
+  const errorHandler = (error, errorInfo) => {
+    console.log('Logging', error, errorInfo);
+  }
+
   return( 
     <div>
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route exact path="/" element={<Home/>} /> 
-          <Route path="/About" element={<About/>} />
+      <ErrorBoundary FallbackComponent={Fallback} onError={errorHandler}>
+        <Router>
+          <Navbar/>
+          <Routes>
+            <Route exact path="/" element={<Home/>} /> 
+            <Route path="/About" element={<About/>} />
           {/*<Route path="/Contact" element={<Contact/>} />*/}
-          <Route path="/Profile/:details" element={<RepoDetails />} />
-          <Route path="/Portfolio" element={<Portfolio/>} />
-          <Route path="*" element={<PageError/>} />
-        </Routes>
-      </Router>
+            <Route path="/Profile/:details" element={<RepoDetails />} />
+            <Route path="/Portfolio" element={<Portfolio/>} />
+            <Route path="*" element={<PageError/>} />
+          </Routes>
+        </Router>
+      </ErrorBoundary>
     </div>
   );
 };
